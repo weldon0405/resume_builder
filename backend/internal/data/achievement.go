144bfd6a8271da86b88fd607f6d5903ed9593648
malbwa <weldon0405@gmail.com>
2025-03-24 06:11:56 -0400

    minor updates

--- backend/internal/data/achievement.go
+++ backend/internal/data/achievement.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"time"
+
+	"github.com/weldon0405/resume_builder/internal/validator"
 )
 
 type Achievement struct {
@@ -11,4 +13,11 @@ type Achievement struct {
 	UserID      int64     `json:"user_id"`
 	Description string    `json:"description"`
 	Skills      []string  `json:"skills,omitzero"`
+}
+
+func ValidateAchievement(v *validator.Validator, achievement *Achievement) {
+	v.Check(achievement.Description != "", "description", "must be provided")
+	v.Check(achievement.PositionID > 0, "position_id", "must associate with a position")
+	v.Check(achievement.Skills != nil, "skills", "must be provided")
+	v.Check(validator.Unique(achievement.Skills), "skills", "must not contain duplicate values")
 }
